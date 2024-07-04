d298a33ed2cde9823a3fa5734164dce21398e573
William-Yao-007 <William.Yao@Copeland.com>
2024-07-04 18:25:00 +0800

    change uart files

--- src/uart.c
+++ src/uart.c
@@ -1,43 +1,65 @@
 #include "queue.h"
+#include "uart.h"
+
 
 //-----------------------------------------------------------------------------
 //
 //-----------------------------------------------------------------------------
-QUEUE_T uart_queue;
-
+QUEUE_T uart0_queue;
+UART_DATA_T uart0_data;
 
 //=============================================================================
 //=============================================================================
-void init_uart(void)
+void init_uart0(void)
 {
     //
-
-    init_queue(&uart_queue);
+    uart0_data.uart_queue = &uart0_queue;
+    init_queue(uart0_data.uart_queue);
 }
 
 //=============================================================================
+// push data into queue buffer
 //=============================================================================
-char uart_receive_byte(QUEUE_T *uq, char byte_value)
+char uart_push_byte(UART_DATA_T *uart_data, char byte_value)
 {
-    return(enqueue(uq, byte_value));
+    return(enqueue(uart_data->uart_queue, byte_value));
 }
 
 //=============================================================================
+// push data into queue buffer
 //=============================================================================
-short uart_receive_data(QUEUE_T *uq, char *byte_data, unsigned short byte_length)
+short uart_push_data(UART_DATA_T *uart_data, char *byte_array, unsigned short byte_length)
 {
     short i = 0;
 
     for(i = 0; i < byte_length; i++) {
-        if(enqueue(uq, byte_data[i]) < 0) return -1;
+        if(enqueue(uart_data->uart_queue, byte_array[i]) < 0) return -1;       // buffer is full
     }
 
     return i;
 }
 
 //=============================================================================
+// pull data from queue buffer
 //=============================================================================
-char uart_transmit_data(QUEUE_T *uq, unsigned short byte_length) {}
+char uart_pull_byte(UART_DATA_T *uart_data, char *byte_variable)
+{
+    return(dequeue(uart_data->uart_queue, byte_variable));
+}
+
+//=============================================================================
+// pull data from queue buffer
+//=============================================================================
+short uart_pull_data(UART_DATA_T *uart_data, char *byte_array, unsigned short byte_length)
+{
+    short i = 0;
+
+    for(i = 0; i < byte_length; i++) {
+        if(dequeue(uart_data->uart_queue, &byte_array[i]) < 0) return -2;       // buffer is empty
+    }
+
+    return i;
+}
 
 //=============================================================================
 //=============================================================================
